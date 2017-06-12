eb2f18c48f639ffca6d3c940af753899d9dd8552
Mohamed Ibrahim <mohamed.ibrahim1@uoit.net>
2017-06-12 18:28:55 +0200

    added ederik to test.rb

--- db/test.rb
+++ db/test.rb
@@ -3,4 +3,4 @@ casa = Flat.new(price: 35, title: "nice flat in barca", description: "slhgaslgha
 booking = Booking.new(start_date: "12/06/2017", end_date: "16/06/2017", guests: 3)
 
 
-ederik = albert = User.new(first_name: "albert", last_name: "montolio", email: "email@email.com", password: "1234567")
+ederik =  User.new(first_name: "albert", last_name: "montolio", email: "email@email.com", password: "1234567")
