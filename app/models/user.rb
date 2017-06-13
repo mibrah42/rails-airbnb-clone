fa9d37842382ba8d8844729c0cc780406c77f3a2
Mohamed Ibrahim <mohamed.ibrahim1@uoit.net>
2017-06-13 11:25:45 +0200

    finished facebook auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :flats, through: :bookings
   has_many :bookings, dependent: :destroy
 
-  validates :first_name, :last_name, presence: true
+  # validates :first_name, :last_name, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
