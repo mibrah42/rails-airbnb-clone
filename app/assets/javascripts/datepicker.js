5066698492fc0088f218ce5526b91e111dbc0975
Albert Montolio <albert.montolio@gmail.com>
2017-06-14 13:44:35 +0200

    autocomplete destination

---
+++ app/assets/javascripts/datepicker.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+  $('.datepicker').datepicker({
+      daysOfWeekDisabled: "0,6",
+      daysOfWeekHighlighted: "1,2,3,4,5",
+      todayHighlight: true
+  });
+
+});
