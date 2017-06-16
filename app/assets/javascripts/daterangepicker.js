e6c19e0df1e39bc092ee0ee3e271bd98a3083191
sbuffosempe <sebastianbuffosempe@gmail.com>
2017-06-16 12:15:45 +0200

    Added style on the daterangepicker

--- app/assets/javascripts/daterangepicker.js
+++ app/assets/javascripts/daterangepicker.js
@@ -1,8 +1,26 @@
 $(document).ready(function(){
+
+
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //January is 0!
+var yyyy = today.getFullYear();
+
+if(dd<10) {
+    dd='0'+dd
+}
+if(mm<10) {
+    mm='0'+mm
+}
+today = mm+'/'+dd+'/'+yyyy;
+
+
   $('#daterange').daterangepicker({
     "timePickerIncrement": 1,
     "opens": "right",
     "drops": "down",
     "autoApply": true,
+    "minDate": today,
   });
+  $("#daterange").val("Anytime")
 });
